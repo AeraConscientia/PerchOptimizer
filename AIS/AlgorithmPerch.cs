bc68fd5d1bd2dd608aeffe56b230c2d59afc1eff
AeraConscientia <annacom84kolessa@yandex.ru>
2021-04-05 13:23:07 +0300

    Initial commit

---
+++ AIS/AlgorithmPerch.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AIS
+{
+    class AlgorithmPerch
+    {
+        public double R1, R2;
+        public double thetta1, thetta2;
+        public double L1, L2;
+        public double x, y;
+
+        private Random rand = new Random();
+        public Perch perch = new Perch();
+
+        /// <summary>Размер популяции окуней </summary>
+        public int population;
+
+        /// <summary>Количество стай</summary>
+        public int NumFlocks = 0;
+        /// <summary>Количество окуней в стае</summary>
+        public int NumPerchInFlock = 0;
+        /// <summary>Количество шагов до окончания движения внутри стаи</summary>
+        public int NStep = 0;
+        /// <summary>Глубина продвижения внутри котла</summary>
+        public double sigma = 0;
+
+        /// <summary>Параметр распределения Леви</summary>
+        public double lambda = 0;
+        /// <summary>Величина шага</summary>
+        public double alfa = 0;
+
+        /// <summary>Число перекоммутаций</summary>
+        public int PRmax = 0;
+        /// <summary>Число шагов при перекоммутации</summary>
+        public int deltapr = 0;
+
+        /// <summary>Номер выбранной функции</summary>
+        public int f;
+
+        /// <summary>Область определения</summary>
+        public double[,] D;
+
+        /// <summary>Максимальное число итераций</summary>
+        public int MaxCount { get; set; }
+
+        /// <summary>Текущая итерация </summary>
+        public int currentIteration = 0;
+
+        /// <summary>Массив средней приспособленности</summary>
+        public List<double> averageFitness = new List<double>();
+
+        /// <summary>Массив лучшей приспособленности</summary>
+        public List<double> bestFitness = new List<double>();
+
+        /// <summary>Популяция окуней</summary>
+        public List<Perch> individuals = new List<Perch>();
+
+        public List<Perch> Pool = new List<Perch>();
+
+        public Perch[,] flock;
+
+        public AlgorithmPerch() { }
+
+        /// <summary>Сортировка ВСЕХ окуней</summary>
+        private void Sort(List<Perch> list) // TODO: протестить на предмет правильно сортировки
+        {
+            for (int i = 0; i < list.Count; i++)
+                for (int j = 0; j < list.Count - i - 1; j++)
+                    if (list[j].fitness > list[j + 1].fitness)
+                    {
+                        Perch tmp = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = tmp;
+                    }
+        }
+
+        /// <summary>Сортировка всех окуней в своей стае</summary>
+        private void Sort(Perch[,] perches, int flockIndex)
+        {
+            for (int i = 0; i < NumPerchInFlock; i++)
+                for (int j = 0; j < NumPerchInFlock - i -1; j++)
+                    if (perches[flockIndex, j].fitness > perches[flockIndex, j+1].fitness)
+                    {
+                        Perch tmp = perches[flockIndex, j];
+                        perches[flockIndex, j] = perches[flockIndex, j + 1];
+                        perches[flockIndex, j + 1] = tmp;
+                    }
+        }
+
+        private void SortFlocks()
+        {
+            for (int i = 0; i < NumFlocks; i++)
+            {
+                for (int j = 0; j < NumFlocks - i - 1; j++)
+                {
+                    if (flock[j,0].fitness > flock[j + 1 , 0].fitness)
+                    {
+                        Perch[] tmp = new Perch[NumPerchInFlock];
+                        for (int k = 0; k < NumPerchInFlock; k++)
+                            tmp[k] = flock[j, k];
+                        for (int k = 0; k < NumPerchInFlock; k++)
+                            flock[j, k] = flock[j + 1, k];
+                        for (int k = 0; k < NumPerchInFlock; k++)
+                            flock[j + 1, k] = tmp[k];
+                    }
+                }
+            }
+        }
+
+        /// <summary>Разбивка окуней на стаи</summary>
+        public void MakeFlocks() 
+        {
+            Sort(individuals);
+            flock = new Perch[NumFlocks, NumPerchInFlock];
+
+            for (int i = 0; i < individuals.Count; i++)
+            {
+                int tmp;
+                Math.DivRem(i, NumFlocks, out tmp);
+                flock[tmp, i / (NumFlocks)] = individuals[i];
+            }
+            
+
+            //flock[0][i] - перебор окуней в первой стае
+            //flock[-1][i] - перебор окуней в первой стае
+
+        }
+
+        /// <summary>Движения каждого окуня в каждой стае</summary>
+        public void MoveEPerchEFlock()
+        {
+            sigma = rand.NextDouble()*0.4 + 0.1; // sigma [0.1,  0.5]
+
+            for (int i = 0; i < NumFlocks; i++)
+            {
+                for (int j = 0; j < NumPerchInFlock; j++)
+                {
+                    double x = 0;
+                    double y = 0;
+                    int moveCount = (int)Math.Floor(sigma * NStep);
+
+                    List<Perch> move = new List<Perch>();
+                    for (int k = 0; k < moveCount; ++k)
+                    {
+                        x = flock[i, j].coords[0] + k * ((flock[i, 0].coords[0] - flock[i, j].coords[0]) / (NStep));
+                        y = flock[i, j].coords[1] + k * ((flock[i, 0].coords[1] - flock[i, j].coords[1]) / (NStep));
+                        move.Add(new Perch(x, y, function(x, y, f)));
+                    }
+                    Sort(move);
+                    flock[i, j] = move[0];
+                }
+                Sort(flock, i);
+            }
+            SortFlocks();
+        }
+
+        private void BestFlockSwim()
+        {
+            sigma = rand.NextDouble() * 0.5 + 1; // sigma [1,  1.5]
+            int i = 0;
+            for (int j = 0; j < NumPerchInFlock; j++)
+            {
+                double x = 0;
+                double y = 0;
+                int moveCount = (int)Math.Floor(sigma * NStep);
+
+                List<Perch> move = new List<Perch>();
+                for (int k = 0; k < moveCount; ++k)
+                {
+                    x = flock[i, j].coords[0] + k * ((flock[i, 0].coords[0] - flock[i, j].coords[0]) / (NStep));
+                    y = flock[i, j].coords[1] + k * ((flock[i, 0].coords[1] - flock[i, j].coords[1]) / (NStep));
+                    move.Add(new Perch(x, y, function(x, y, f)));
+                }
+                Sort(move);
+                flock[i, j] = move[0];
+            }
+            Sort(flock, i);
+        }
+
+        private void PoorFlockSwim()
+        {
+            sigma = rand.NextDouble() * 0.4 + 0.1; // sigma [0.1,  0.5]
+            int i = 1;
+
+
+            for (int j = 0; j < NumPerchInFlock; j++)
+            {
+                double x = 0;
+                double y = 0;
+                int moveCount = (int)Math.Floor(sigma * NStep);
+
+                List<Perch> move = new List<Perch>();
+                for (int k = 0; k < moveCount; ++k)
+                {
+                    x = flock[i, j].coords[0] + k * ((flock[i, 0].coords[0] - flock[i, j].coords[0]) / (NStep));
+                    y = flock[i, j].coords[1] + k * ((flock[i, 0].coords[1] - flock[i, j].coords[1]) / (NStep));
+                    move.Add(new Perch(x, y, function(x, y, f)));
+                }
+                Sort(move);
+                flock[i, j] = move[0];
+            }
+            Sort(flock, i);
+        }
+
+        /// <summary>Распределение Леви для худшей стаи</summary>
+        public void Levi()
+        {
+            R1 = rand.Next(Convert.ToInt32(D[0,0] * 100), Convert.ToInt32(D[0,1] * 100)) / 100f;
+            R2 = rand.Next(Convert.ToInt32(D[1, 0] * 100), Convert.ToInt32(D[1, 1] * 100)) / 100f;
+            thetta1 = R1 * 2 * Math.PI;
+            thetta2 = R2 * 2 * Math.PI;
+
+            L1 = Math.Pow(R1, -1 / lambda);
+            L2 = Math.Pow(R2, -1 / lambda);
+
+            x = L1 * Math.Sin(thetta1);
+            y = L2 * Math.Cos(thetta2); // TODO: странна, менять, если что
+
+        }
+
+        /// <summary>Начальное формирование популяции </summary>
+        public void FormingPopulation()
+        {
+            for (int i = 0; i < population; i++)
+            {
+                x = 0;
+                y = 0;
+
+                x = D[0, 0] + rand.NextDouble() * (D[0, 1] - D[0, 0]);
+                y = D[1, 0] + rand.NextDouble() * (D[1, 1] - D[1, 0]);
+
+                Perch perch = new Perch(x, y, function(x, y, f)); // TODO: добавить iter += 1
+                individuals.Add(perch);
+            }
+        }
+
+        public Perch StartAlg(int MaxCount, double[,] D, int f, 
+            int NumFlocks, int NumPerchInFlock, 
+            int NStep, double sigma, 
+            double lambda, double alfa, 
+            int PRmax, int deltapr)
+        {
+            this.MaxCount = MaxCount;
+
+            this.D = D;
+            this.f = f;
+
+            this.NumFlocks = NumFlocks;
+            this.NumPerchInFlock = NumPerchInFlock;
+            population = NumFlocks * NumPerchInFlock;
+
+            this.NStep = NStep;
+            this.sigma = sigma;
+            this.lambda = lambda;
+            this.alfa = alfa;
+            this.PRmax = PRmax;
+            this.deltapr = deltapr;
+
+            FormingPopulation();
+            MakeFlocks();
+            MoveEPerchEFlock();
+
+            return perch;
+        }
+
+        private float function(double x1, double x2, int f)
+        {
+            float funct = 0;
+            if (f == 0)
+            {
+                funct = (float)(x1 * Math.Sin(Math.Sqrt(Math.Abs(x1))) + x2 * Math.Sin(Math.Sqrt(Math.Abs(x2))));
+            }
+            else if (f == 1)
+            {
+                funct = (float)(x1 * Math.Sin(4 * Math.PI * x1) - x2 * Math.Sin(4 * Math.PI * x2 + Math.PI) + 1);
+            }
+            else if (f == 2)
+            {
+                double[] c6 = Cpow(x1, x2, 6);
+                funct = (float)(1 / (1 + Math.Sqrt((c6[0] - 1) * (c6[0] - 1) + c6[1] * c6[1])));
+            }
+            else if (f == 3)
+            {
+                funct = (float)(0.5 - (Math.Pow(Math.Sin(Math.Sqrt(x1 * x1 + x2 * x2)), 2) - 0.5) / (1 + 0.001 * (x1 * x1 + x2 * x2)));
+            }
+            else if (f == 4)
+            {
+                funct = (float)((-x1 * x1 + 10 * Math.Cos(2 * Math.PI * x1)) + (-x2 * x2 + 10 * Math.Cos(2 * Math.PI * x2)));
+            }
+            else if (f == 5)
+            {
+                funct = (float)(-Math.E + 20 * Math.Exp(-0.2 * Math.Sqrt((x1 * x1 + x2 * x2) / 2)) + Math.Exp((Math.Cos(2 * Math.PI * x1) + Math.Cos(2 * Math.PI * x2)) / 2));
+            }
+            else if (f == 6)
+            {
+                funct = (float)(Math.Pow(Math.Cos(2 * x1 * x1) - 1.1, 2) + Math.Pow(Math.Sin(0.5 * x1) - 1.2, 2) - Math.Pow(Math.Cos(2 * x2 * x2) - 1.1, 2) + Math.Pow(Math.Sin(0.5 * x2) - 1.2, 2));
+            }
+            else if (f == 7)
+            {
+                funct = (float)(-Math.Sqrt(Math.Abs(Math.Sin(Math.Sin(Math.Sqrt(Math.Abs(Math.Sin(x1 - 1))) + Math.Sqrt(Math.Abs(Math.Sin(x2 + 2))))))) + 1);
+            }
+            else if (f == 8)
+            {
+                funct = (float)(-(1 - x1) * (1 - x1) - 100 * (x2 - x1 * x1) * (x2 - x1 * x1));
+            }
+            else if (f == 9)
+            {
+                funct = (float)(-x1 * x1 - x2 * x2);
+            }
+            return funct;
+        }
+        private double[] Cpow(double x, double y, int p)
+        {
+            double[] Cp = new double[2];
+            Cp[0] = x; Cp[1] = y;
+            double x0 = 0;
+            double y0 = 0;
+            for (int i = 1; i < p; i++)
+            {
+                x0 = Cp[0] * x - Cp[1] * y;
+                y0 = Cp[1] * x + Cp[0] * y;
+                Cp[0] = x0; Cp[1] = y0;
+            }
+            return Cp;
+        }
+        public double AverageFitness()
+        {
+            double sum = 0;
+            for (int i = 0; i < population; i++)
+                sum += individuals[i].fitness;
+            double fitness = (sum / population);
+            averageFitness.Add(fitness);
+            return fitness;
+        }
+    }
+}
---
+++ AIS/FormStepPerch.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace AIS
+{
+    partial class FormStepPerch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormStepPerch));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 61);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Генерация начальной популяции";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(24, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 61);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Деление популяции на стаи";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(24, 208);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 61);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Реализация окуневого котла в стаях";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(24, 301);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(127, 61);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Плавание стай";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(24, 393);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(127, 61);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "Помещение лидера популяции в множество Pool";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(206, 184);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(117, 61);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "Проверка условий завершения поиска";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(206, 515);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(127, 60);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "Интенсивный поиск в Pool";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(24, 515);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(127, 60);
+            this.button8.TabIndex = 1;
+            this.button8.Text = "Выбор наилучшего решения";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(349, 584);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormStepPerch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1213, 633);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FormStepPerch";
+            this.Text = "FormStepPerch";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
