35b3d140af8f836cd45fa3c49385205ab0f22407
AeraConscientia <annacom84kolessa@yandex.ru>
2022-05-26 15:19:17 +0300

    Визуальное улучшение
    Можно прокручивать окна при маленьком разрешении.
    Мелочь, а кому-то приятно.

--- AIS/FormStepPerch.Designer.cs
+++ AIS/FormStepPerch.Designer.cs
@@ -432,6 +432,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(1262, 667);
             this.Controls.Add(this.buttonSavePictures);
             this.Controls.Add(this.label4);
